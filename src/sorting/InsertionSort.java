fc084c8cf18be51f57d7dbe83ed2af5f1f1866ef
vinayakrdeshpande <15637546+vinayakrdeshpande@users.noreply.github.com>
2020-08-15 23:36:34 +0530

    insertion and selection sort

--- src/sorting/InsertionSort.java
+++ src/sorting/InsertionSort.java
@@ -1,7 +1,33 @@
 package sorting;
 
+import java.util.Arrays;
+
 /**
+ * Step 1: Default 1st element is sorted will be added in sorted Array.
+ * Step 2: Second element will be compared in the sorted Array and will added accordingly
+ * Step 3: this will be executed in loop till new sorted array has all the fields compared to the existing
  *
+ * Ascending <, Descending >
+ * Stable sort as we are not using =, so the same values willn't be swapped.
+ *
+ * TimeComplexity: o(n2) Quadratic TimeComplexity
  */
 public class InsertionSort {
+    public static void main(String[] args) {
+        int[] a = {20, 35, -15, 7, 55, 1, -22};
+
+        for(int firstUnsortedIndex=1; firstUnsortedIndex<a.length; firstUnsortedIndex++){
+            int newElement= a[firstUnsortedIndex];
+            int i;
+            for(i=firstUnsortedIndex; i>0 && a[i-1]>newElement;i--){
+                a[i]=a[i-1];
+            }
+               a[i] = newElement;
+        }
+
+        Arrays.stream(a).forEach(n->{
+            System.out.print(n);
+            System.out.print(", ");
+        });
+    }
 }
--- src/sorting/SelectionSort.java
+++ src/sorting/SelectionSort.java
@@ -1,4 +1,43 @@
 package sorting;
 
+import com.sun.scenario.effect.impl.sw.java.JSWColorAdjustPeer;
+
+import java.util.Arrays;
+
+/**
+ * Assuming Ascending
+ * Step 1: Default 1st element is maximum.
+ * Step 2: Compare tis value with other values if found maximum swap the values
+ * Step 3: this will be executed in loop till new sorted array has all the fields compared to the existing
+ *
+ * Ascending <, Descending >
+ * Stable sort as we are not using =, so the same values willn't be swapped.
+ *
+ * TimeComplexity: o(n2) Quadratic TimeComplexity
+ */
 public class SelectionSort {
+
+    public static void main(String[] args) {
+        int[] a = {20, 35, -15, 7, 55, 1, -22};
+
+        for(int lastUnsortedIndex=a.length-1; lastUnsortedIndex>0; lastUnsortedIndex--){
+            int largest= 0;
+            for (int i = 1; i <=lastUnsortedIndex ; i++) {
+                if(a[i]>a[largest]) //for ascending
+                    largest=i;
+            }
+            swapValues(a, largest, lastUnsortedIndex);
+        }
+        Arrays.stream(a).forEach(n->{
+            System.out.print(n+", ");
+        });
+    }
+
+    private static void swapValues(int[] a, int largest, int lastUnsortedIndex) {
+        int temp=a[largest];
+        a[largest]=a[lastUnsortedIndex];
+        a[lastUnsortedIndex]=temp;
+    }
+
+
 }
